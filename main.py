21a0748b6f7095b4ecfc942d5c1ec5e3bda8befd
Gabriel Dickmann Schneider <thecueionerd@gmail.com>
2023-02-16 21:30:21 -0300

    implementando o reconhecimento de voz

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ r = sr.Recognizer()
 
 #Abrir o microfone ara captura
 with sr.Microphone() as source:
-  audio = r.listen(source) #Define o microfone como fonte de áudio
+  while True:
+     audio = r.listen(source) #Define o microfone como fonte de áudio
 
-  print(r.recognize_google(audio))
+     print(r.recognize_google(audio, language="pt-BR")) #Acessa o reconhecedor e retorna o texto
+
